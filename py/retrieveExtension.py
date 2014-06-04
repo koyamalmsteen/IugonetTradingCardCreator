cc8a57b278b869c66723296601a32220ca6873e4
iugonet <iugonet@localhost.localdomain>
2014-06-04 21:24:16 +0900

    Fixed

--- py/retrieveExtension.py
+++ py/retrieveExtension.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python
-from xml.etree import ElementTree
-import getopt, sys, Image
+try:
+    import lxml.etree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+
+import optparse
+import sys
+import Image
 
 def main():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
-    except getopt.GetoptError:
-        usage()
-        sys.exit(2)
-    output = None
-    verbose = False
-    for o, a in opts:
-        if o == "-v":
-            verbose = True
-        if o in ("-h", "--help"):
-            usage()
-            sys.exit()
-        if o in ("-o", "--output"):
-            output = a
+    parser = optparse.OptionParser()
+    parser.add_option('-i', '--input', 
+                      dest="input_filename",
+                      default="../xml/VHF.xml",
+                      help="(in XML)")
+    parser.add_option('-o', '--output',
+                      dest="output_filename",
+                      default="/tmp/qrcode.png",
+                      help="(in PNG)")
+    options, remainder = parser.parse_args()
 
-    FILE = "qrcode.png"
+    tree = ET.parse(options.input_filename)
+    elem = tree.getroot()
 
-    Image.open(FILE).resize((300,150)).save("qrcode2.png")
+    id = elem.find('.//{http://www.iugonet.org/data/schema}Instrument/{http://www.iugonet.org/data/schema}ResourceID').text
+
+    img = qrcode.make(id)
+#    img = qrcode.make(id, image_factory=qrcode.image.svg.SvgImage)
+    img.save(options.output_filename)
+
+# http://center.stelab.nagoya-u.ac.jp/sd2007/top.jpg
+    Image.open(options.input_filename).resize((300,150)).save("qrcode2.png")
 
 if __name__ == "__main__":
     main()
