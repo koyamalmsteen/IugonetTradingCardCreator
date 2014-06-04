11c113a87db5bc793226a705315b71589f257bc2
root <root@h155.89.226.10.20871.vlan.kuins.net>
2014-06-04 18:08:26 +0900

    Fixed

---
+++ xml/retrieveExtension.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from xml.etree import ElementTree
+import getopt, sys
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    output = None
+    verbose = False
+    for o, a in opts:
+        if o == "-v":
+            verbose = True
+        if o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        if o in ("-o", "--output"):
+            output = a
+
+    XMLFILE = "VHF.xml"
+
+    tree = ElementTree.parse(XMLFILE)
+    elem = tree.getroot()
+
+    print elem.tag
+    print elem.attrib
+
+#    e = elem.find('.//Version')
+#    print e.text
+
+if __name__ == "__main__":
+    main()
